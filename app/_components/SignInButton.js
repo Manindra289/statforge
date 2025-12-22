a8837e22be6ae434ac5971ea2784c4658ee678d6
Manindra289 <manindrakhandyana@gmail.com>
2025-12-22 11:01:54 +0530

    view demo button

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -4,7 +4,7 @@ import { demoSignInAction, signInAction } from "../_lib/actions";
 
 function SignInButton() {
   return (
-    <>
+    <div className="flex flex-row gap-3">
     <form action={signInAction}>
 
     <button className='flex items-center gap-6 px-10 py-4  text-lg border border-black hover:border-gray-600 rounded-lg font-bold'>
@@ -17,15 +17,14 @@ function SignInButton() {
       <span>Continue with Google</span>
     </button>
     </form>
-    <br/>
 
-    <form action={demoSignInAction} className="mt-4">
-      <button className="bg-yellow-400 px-8 py-6 text-white-800 text-lg font-semibold rounded-lg
+    <form action={demoSignInAction}>
+      <button className="bg-yellow-400 px-8 py-3 text-white-800 text-lg font-semibold rounded-lg
         hover:bg-yellow-500 transition-all max-sm:text-lg max-sm:px-4 max-sm:py-3 ">
         View Demo
       </button>
     </form>
-    </>
+    </div>
   )
 }
 
